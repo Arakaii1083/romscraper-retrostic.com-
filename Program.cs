353bbe8195305c2f83aa6449d9bf424498d51214
Martinez <antonio.martinez@hp.com>
2020-05-17 18:59:29 +0200

    backup 17-05-20

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net.Http;
+using static System.Console;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net;
+using System.IO;
+
+namespace romscraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /******URI FETCHER******/
+            // HttpClient client = new HttpClient();
+            // var response = await client.GetAsync("https://www.freeroms.com/");
+            // var pageContents = await response.Content.ReadAsStringAsync();
+
+            /******LIST FETCHER******/
+            // var web = @"source\index2.html";
+
+            // var pageDocument = new HtmlDocument();
+            // pageDocument.Load(web);
+            
+            // var listLinks = pageDocument.DocumentNode.SelectNodes("//ul[@class='desktop-menu']/li/a");
+            // foreach(var node in listLinks){
+            //     if(node.InnerText!="Links" || node.InnerText!="Flash Games"){
+            //         Console.WriteLine(node.Attributes["href"].Value);
+            //     }
+            // }
+
+            /******INDEX FETCHER******/
+            // var web2 = @"source/index_amiga.html";
+
+            // var pageDocumentPlatform = new HtmlDocument();
+            // pageDocumentPlatform.Load(web2);
+
+            // var listLinks = pageDocumentPlatform.DocumentNode.SelectNodes("//div[@class='page']/a");
+            // foreach(var node in listLinks){
+            //     Write(node.InnerText + ": ");
+            //     Console.WriteLine(node.Attributes["href"].Value);
+            // }
+
+            /******ROM/URL FETCHER******/
+            int count = 0;
+            var web3 = @"source/index_amiga_a.html";
+
+            var pageDocumentPlatformIndex = new HtmlDocument();
+            pageDocumentPlatformIndex.Load(web3);
+
+            var listLinks = pageDocumentPlatformIndex.DocumentNode.SelectNodes("//div[@class='rom-tr title']/a");
+            foreach(var node in listLinks){
+                var html = @"https://www.freeroms.com/"+node.Attributes["href"].Value;
+                HtmlWeb htmlweb = new HtmlWeb();
+                var htmlRom = htmlweb.Load(html);
+                var romLink = htmlRom.DocumentNode.SelectSingleNode("//script[@language='javascript']");
+                string romScript = romLink.OuterHtml;
+                int lengthSubs = romScript.IndexOf(".zip") - romScript.IndexOf("http");
+                string urlDownload = romScript.Substring(romScript.IndexOf("http"),lengthSubs+4);
+                WebClient wClient = new WebClient();
+                wClient.DownloadFile(urlDownload, @"prueba/" + node.InnerText + ".zip");
+                count++;
+                WriteLine($"Downloaded: {count}");
+                
+                // Write(node.InnerText + ": ");
+                // Console.WriteLine(node.Attributes["href"].Value);
+            }
+
+            /******ROM/SCRIPT/URL EXTRACTOR******/
+            // var web4 = @"source/index_rom_test.html";
+
+            // var pageDocumentPlatformIndex = new HtmlDocument();
+            // pageDocumentPlatformIndex.Load(web4);
+
+            // var romLink = pageDocumentPlatformIndex.DocumentNode.SelectSingleNode("//script[@language='javascript']");
+            // string romScript = romLink.OuterHtml;
+
+            // int lengthSubs = romScript.IndexOf(".zip") - romScript.IndexOf("http");
+            // string urlDownload = romScript.Substring(romScript.IndexOf("http"),lengthSubs+4);
+
+            // /******DOWNLOADER******/
+
+            // WebClient wClient = new WebClient();
+            // wClient.DownloadFile(urlDownload, @"prueba/rom.zip");
+        }
+    }
+}
